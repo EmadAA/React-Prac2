15d7877e40e2dcc1292f9b6861cdb47f6a660078
EmadAA <emadadil999@gmail.com>
2024-01-16 00:49:23 +0600

    2nd Commmit

--- React+Vite/src/App.jsx
+++ React+Vite/src/App.jsx
@@ -1,33 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/* eslint-disable no-unused-vars */
+
+import React from 'react'
+import Card from './Components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React  with  Rabbil hasan Rupom Bhai</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1 className='text-center text-bg-danger '>Hello Adil How are you </h1>
+    <Card/>
     </>
   )
 }
