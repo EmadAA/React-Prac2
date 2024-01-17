25442ffafc7fa364736878b6961d3bf0e2485bc1
EmadAA <emadadil999@gmail.com>
2024-01-18 00:46:00 +0600

    last

--- React+Vite/src/Components/Card.jsx
+++ React+Vite/src/Components/Card.jsx
@@ -5,7 +5,8 @@ import React from 'react';
 const Card = () => {
     return (
        
-         <div className="card w-25 ">
+         <div className='container row-cols-lg-4 '>
+<div className="card w-25 ">
             <img src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" className="card-img-top" alt="demo images" />
             <div className="card-body">
                 <h5 className="card-title">Card Title</h5>
@@ -13,6 +14,15 @@ const Card = () => {
                 <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
+        <div className="card w-25 ">
+            <img src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" className="card-img-top" alt="demo images" />
+            <div className="card-body">
+                <h5 className="card-title">Card Title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, autem? Earum dolore repudiandae officia nisi ad qui sunt voluptatem. Iste.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+         </div>
        
     );
 };
